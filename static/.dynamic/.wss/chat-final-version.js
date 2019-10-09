0c874324af8d9830f876817fca23e92cd6230b7f
Aral Balkan <mail@ar.al>
2019-10-09 13:15:48 +0100

    Add final version of chat server script

---
+++ static/.dynamic/.wss/chat-final-version.js
@@ -0,0 +1,27 @@
+module.exports = function (client, request) {
+  // New client connection: persist client’s “room”
+  // based on request path.
+  client.room = this.setRoom(request)
+
+  // Log the connection.
+  console.log(`New client connected to ${client.room}`)
+
+  client.on('message', message => {
+
+    // Perform some basic validation.
+    if (message.nickname === '' || message.text === '') {
+      console.log(`Missing nickname or message; not broadcasting.`)
+      return
+    }
+
+    // New message received: broadcast it to all
+    // other clients in the same room.
+    const numberOfRecipients = this.broadcast(client, message)
+
+    // Log the number of recipients message was sent to
+    // and make sure we pluralise the log message properly.
+    console.log(`${client.room} message broadcast to `
+      + `${numberOfRecipients} recipient`
+      + `${numberOfRecipients === 1 ? '' : 's'}`)
+  })
+}
